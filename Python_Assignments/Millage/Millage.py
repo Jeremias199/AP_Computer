843ba3334e113aa23ec029d6f8730797cf36cc8d
s1098659 <s1098659@student.knoxschools.org>
2026-01-22 15:15:44 -0500

    New Code 1/22/2026

---
+++ Python_Assignments/Millage/Millage.py
@@ -0,0 +1,7 @@
+print("Hello!")
+
+Ask_Miles = input("How many miles did you travel from here? ")
+Ask_Gallons = input("How many gallons did you have before you got here? ")
+
+print("Your MPG is " , (Ask_Miles / Ask_Gallons))
+
---
+++ Python_Prartice/Hello_World.py
@@ -0,0 +1,13 @@
+Ask_Print = input("What do you want to print: ");
+
+if Ask_Print == "int":
+    #Integers
+    num1 = 2
+    num2 = 8
+elif Ask_Print == "string":
+    #Strings
+    print("Help" , num2 + num1)
+
+
+
+
