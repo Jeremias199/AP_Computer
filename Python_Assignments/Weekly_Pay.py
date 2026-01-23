f466ab35ac3dbb68ce46ce4a23fd85821fd0a98b
Nerd_Destroyer <s1098659@student.knoxschools.org>
2026-01-23 12:14:24 -0500

    Weekly Pay Calculator
    1/23/2026 - Calculated weekly pay

---
+++ Python_Assignments/Weekly_Pay.py
@@ -0,0 +1,8 @@
+print("Lets calculate your weekly pay!")
+
+Hourly_Wage = input("What's your hourly wage: ")
+Hours_Worked = input("How many hours did you work: ")
+Weekly_Pay = 0;
+
+Weekly_Pay = int(Hourly_Wage) * int(Hours_Worked)
+print("Your weekly pay is $", Weekly_Pay)
