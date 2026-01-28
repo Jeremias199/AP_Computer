57b1f34e32bbbca8e71113ebf677f2820340865c
s1098659 <s1098659@student.knoxschools.org>
2026-01-28 15:01:18 -0500

    Reorganized assignments from AP Comp Sci to their assignments.

--- Python_Prartice/Greeting.py
+++ Python_Prartice/Greeting.py
@@ -1,3 +1,20 @@
 Student_Name = input("What is your name?")
 
 print(Student_Name)
+
+Class = input("What is your favorite class")
+print(Class)
+
+num1 = int(input("Type your first number: "))
+num2 = int(input("Type your second number: "))
+
+Sum = num1 + num2
+
+print(Sum)
+
+age = int(input("What is your age"))
+
+if age >= 16:
+    print("You can drive!")
+else:
+    print("YOU CANT DRIVE!")
