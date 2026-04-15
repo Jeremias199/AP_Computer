25b0e7df1e1d7fdd6c1e722e8097e1f19c390a0b
jeremias <s1098659@student.knoxschools.org>
2026-04-14 21:59:11 -0400

    Tue/Apr 14/ 2026 9:56pm Commit Name: BOUNDS AND REPEATING File Name: Random_Number.py Comments: I created a less redundant way of getting a low and high bounds when generating a random number. Which I created it using one input, and collecting them as a string. While I added a new feature that'll ask the user if they would want a repeating random generator.

--- Python_Assignments/Create/Random_Number.py
+++ Python_Assignments/Create/Random_Number.py
@@ -1,30 +1,31 @@
 import random
 
 Samples_UI = int(input("How many random samples do you need? "))
-Low_Bound = int(input("Lowest number in your random number generator: "))
-High_Bound = int(input("Highest number in your random number generator: "))
-
-#list_stuff will list the order of each random number starting from one.
-list_stuff = []
-#rand_list will list a random number according to the number of random samples.
-rand_list = []
-
-Samples_UI = int(input("How many random samples do you need? "))
-Low_Bound = int(input("Lowest number in your random number generator: "))
-High_Bound = int(input("Highest number in your random number generator: "))
+Repeating_Num = input("Would you like repeating numbers? (y/n)").lower()
+Bounds = input("Random number bounds (low and high) separate with space or comma: ")
+bounds_list = Bounds.replace(',', ' ').split()
+Low = int(bounds_list[0])
+High = int(bounds_list[1])
 
 
-def generator(amt_looped, high, low,):
+def generator(amt_looped, high, low, repeating):
     if amt_looped >= 10000:
         print("TOO MANY SAMPLES")
     elif amt_looped < 10000:
-        #This will loop according to the number of samples the user will need.
-        for i in range(amt_looped):
-            #This will start the list from one then it'll continue adding one after each iteration.
-            list_stuff.append(i+1)
-            #This will generate a random number with a buffer from the lowest number to the highest number.
-            Random = random.randint(low, high)
-            rand_list.append(Random)
+        #for i in range(amt_looped):
+        rand_list = []
+        list_stuff = []
+        if repeating == "n" and amt_looped > (high-low+1):
+            print("IMPOSSIBLE GENERATION")
+            return
+
+        if repeating == "n" and amt_looped <= high:
+            rand_list = random.sample(range(low,high +1),amt_looped)
+        elif repeating == "y":
+            for i in range(amt_looped):
+                rand_list.append(random.randint(low,high+1))
+
+        list_stuff = list(range(1,amt_looped + 1))
         print(f"\nSample Number: {list_stuff} \nRandom Number: {rand_list}")
 
-generator(Samples_UI, High_Bound, Low_Bound)
+generator(Samples_UI,High, Low, Repeating_Num)
